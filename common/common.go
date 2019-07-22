4b9590f12b7f711c05e83ce406da4705b2be741f
Unknown <rutmir@mail.ru>
2019-07-22 19:53:46 +0300

    added new functionality

--- common/common.go
+++ common/common.go
@@ -1,9 +1,8 @@
 package common
 
 import (
+	"fmt"
 	"os"
-
-	"github.com/rutmir/go-core/logger"
 )
 
 // GetPropertyOrDefault ...
@@ -19,7 +18,22 @@ func GetPropertyOrDefault(propName, defaultValue string) string {
 func GetRequiredProperty(propName string) string {
 	var result string
 	if result = os.Getenv(propName); len(result) == 0 {
-		logger.Fatal("Environment parameter required", propName)
+		panic(fmt.Sprintf("Environment parameter required %s", propName))
 	}
 	return result
+}
+
+// UniqueStrSlice returns a unique subset of the string slice provided.
+func UniqueStrSlice(input []string) []string {
+	u := make([]string, 0, len(input))
+	m := make(map[string]bool)
+
+	for _, val := range input {
+		if _, ok := m[val]; !ok {
+			m[val] = true
+			u = append(u, val)
+		}
+	}
+
+	return u
 }
